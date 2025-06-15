45dba6b9d35c4d7532eedbe00fbd81b75ad7690a
“parn” <titinanjaem@gmail.com>
2025-06-15 16:33:33 +0700

    add integration test

--- app/controllers/brag_controller.rb
+++ app/controllers/brag_controller.rb
@@ -1,7 +1,4 @@
 class BragController < ApplicationController
   def index
   end
-
-  def show
-  end
 end
--- spec/requests/brag_spec.rb
+++ spec/requests/brag_spec.rb
@@ -7,12 +7,4 @@ RSpec.describe "Brags", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/brag/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
